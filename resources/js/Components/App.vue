1cda5e74492df9bb666652a5562d0c9ac964253e
Nastya0605 <nastya06051981@gmail.com>
2024-06-28 16:21:55 +0300

    First commit

---
+++ resources/js/Components/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container mx-auto">
+        <header>
+            <h1>Laracasts</h1>
+        </header>
+
+        <main class="flex">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link> |
+            <router-view></router-view>
+        </main>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
